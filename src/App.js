e4d19fd94ef0af364c4ac951832de0af7342671c
kaitouz <thienkaito@gmail.com>
2022-11-11 03:46:05 +0300

    created all pages/components & done home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Home } from './pages/home/Home';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { Home } from "./pages/home/Home";
+import { Portfolio } from "./pages/portfolio/Portfolio";
+import { Contact } from "./pages/contact/Contact";
+import { About } from "./pages/about/About";
+
+import "./App.css";
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </div>
+    </BrowserRouter>
   );
 }
 
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,28 +1,54 @@
 import React from 'react'
 import './home.css'
+import Typewriter from "typewriter-effect";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   return (
     <section id="home" className="home">
         
         <div className="intro_sec d-block d-lg-flex align-items-center ">
-            <div
-                className="h_bg-image order-1 order-lg-1"
+            <div className="hero-image order-lg-1"
                 style={{ backgroundImage: `url(${'/hero.png'})` }}>
             </div>
 
-            <div className="text order-2 order-lg-2 d-lg-flex justify-content-center">
-                <div className="align-self-center ">
-                <div className="intro mx-auto">
-                    <div>
-                    
-                    <h2>I'm Thien Le</h2>
-                    <h1>"I'm currently studying at ITMO University"</h1>
-                    </div>
-                    <p>"description"</p>
+            <div className="content order-lg-2 justify-content-center align-self-center">
+              <div className="intro">
+                <div>
+                  <h2>I'm Thien Le</h2>
+                  <h1>
+                    <Typewriter
+                      options={{
+                        strings: [
+                          "I'm currently studying at ITMO University",
+                          "My specialization is Software Engineering",
+                          "I'm digging into Deep Learning and Big Data",
+                        ],
+                        autoStart: true,
+                        loop: true,
+                        deleteSpeed: 10,
+                      }}
+                    />
+                  </h1>
                 </div>
-            </div>
-            </div>
+                <p>"description"</p>
+
+                <div className="intro_btn-action pb-5">
+                  <Link to="/about">
+                    <div id="button_white" className="ac_btn btn ">
+                      About me
+                    </div>
+                  </Link>
+                  <Link to="/contact">
+                    <div id="button_black" className="ac_btn btn">
+                      Contact Me
+                    </div>
+                  </Link>
+                  
+                </div>
+              </div>
+          </div>
+
 
         </div>
     </section>
